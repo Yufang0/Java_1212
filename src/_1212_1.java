7e64d66e0c388076364d7abd424d5ea8fe988368
le404An140\uer <411422259@m365.fju.edu.tw>
2023-12-12 10:29:51 +0800

    排序骰子大小

--- src/_1212_1.java
+++ src/_1212_1.java
@@ -4,16 +4,29 @@ public class _1212_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		int [] array = new int[4];
-		boolean ok=true;
-		for(int i=1;i<=4;i++) {
+		int [] array = new int[4];//用來保存四顆骰子的點數
+		boolean ok=true;//ok為true 代表沒有發生四顆骰子點數全部相同
+		for(int i=1;i<=4;i++) 
 			System.out.printf("The #%d dice: %d\n",i,array[i-1] = dice());
-		}
+		sort(array);
+		for(int i=0;i<4;i++)
+			System.out.printf("The #%d dice: %d\n",i+1,array[i] );
 			
 		
 
 	}
-	public static int dice() {
+	public static void sort(int [] a) {
+		for(int i=0;i<a.length;i++)
+			for(int j=i+1;j<a.length;j++)
+				if(a[i]>=a[j]) {
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+					
+				
+	}
+	public static int dice() {//用來模擬丟一顆骰子的副程式。被呼叫就會產生介於1-6的整數並回傳。
 		return((int)(Math.random()*6+1));
 	}
 
