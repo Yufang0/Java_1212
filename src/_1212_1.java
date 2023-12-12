6de064d1626653a1a03e0d03cb8958eae98e75c1
le404An140\uer <411422259@m365.fju.edu.tw>
2023-12-12 10:04:52 +0800

    丟四次骰子，並儲存於陣列。

--- src/_1212_1.java
+++ src/_1212_1.java
@@ -1,9 +1,20 @@
-
+import java.util.*;
 public class _1212_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int [] array = new int[4];
+		boolean ok=true;
+		for(int i=1;i<=4;i++) {
+			System.out.printf("The #%d dice: %d\n",i,array[i-1] = dice());
+		}
+			
+		
 
+	}
+	public static int dice() {
+		return((int)(Math.random()*6+1));
 	}
 
 }
